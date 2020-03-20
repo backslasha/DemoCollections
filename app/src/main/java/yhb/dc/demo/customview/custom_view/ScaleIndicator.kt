4873d7e884e57a3121eac73926dc15bd61090c44
yaohaibiao <yaohaibiao@bigo.sg>
2020-03-20 14:44:47 +0800

    add ruler demo - draw text logic.

--- app/src/main/java/yhb/dc/demo/customview/custom_view/ScaleIndicator.kt
+++ app/src/main/java/yhb/dc/demo/customview/custom_view/ScaleIndicator.kt
@@ -1,5 +1,7 @@
 package yhb.dc.demo.customview.custom_view
 
+import android.animation.Animator
+import android.animation.ValueAnimator
 import android.annotation.SuppressLint
 import android.content.Context
 import android.graphics.*
@@ -10,6 +12,7 @@ import android.view.MotionEvent.*
 import android.view.View
 import android.view.ViewGroup
 import android.view.ViewGroup.LayoutParams.MATCH_PARENT
+import android.view.animation.DecelerateInterpolator
 import yhb.dc.R
 
 
@@ -32,6 +35,11 @@ class ScaleIndicator @JvmOverloads constructor(
     private val textPaint = Paint()
 
     private var isTouching = false
+    private var animator: Animator? = null
+
+    private val textRect = Rect()
+    private val centerYs = ArrayList<Float>()
+    private val contents = ArrayList<String>()
 
     init {
         // update attrs
@@ -57,42 +65,60 @@ class ScaleIndicator @JvmOverloads constructor(
 
     override fun onDraw(canvas: Canvas?) {
         canvas?.let {
-            drawLine(canvas)
-            drawNode(canvas)
-            drawText(canvas.apply {
-                translate(width / 2 + currentNodeRadius, 0f)
-            })
+
+            centerYs.clear()
+            textRect.set(0, 0, 0, 0)
+            contents.clear()
+
+            var value = maxValue
+            val interval = (maxValue - minValue) / intervalCount
+
+            for (i in 0..intervalCount) {
+                centerYs.add(height - height * (value / maxValue) + nodeRadius / 2)
+                contents.add(content(value))
+                value -= interval
+            }
+
+            drawLine(canvas, width / 2 - linePaint.strokeWidth / 2)
+            drawNode(canvas, width / 2 - linePaint.strokeWidth / 2, centerYs)
+            drawText(canvas, width / 2 - currentNodeRadius, centerYs, contents)
         }
     }
 
-    private fun drawText(canvas: Canvas) {
+    private fun drawText(canvas: Canvas, cx: Float, cys: List<Float>, contents: List<String>) {
+        for (index in cys.indices) {
+            textPaint.textSize = dp2px(context, 26f).toFloat()
+            textPaint.getTextBounds(contents[index], 0, contents[index].length, textRect)
+            canvas.drawText(contents[index], cx - textRect.right - textRect.left - textRect.left,
+                    cys[index] + ((textRect.bottom - textRect.top) / 2).toFloat(), textPaint)
+        }
         // draw current Text
+        canvas.translate(width / 2 + currentNodeRadius, 0f)
+        val content = content(currentValue)
         val cy = height - height * (currentValue / maxValue) + currentNodeRadius / 2
-        val content = currentValue.toString()
-        val rect = Rect()
-        textPaint.getTextBounds(content, 0, content.length, rect)
-        canvas.drawText(content, -rect.left.toFloat(), cy + ((rect.bottom - rect.top) / 2).toFloat(), textPaint)
+        textPaint.textSize = dp2px(context, 30f).toFloat()
+        textPaint.getTextBounds(content, 0, content.length, textRect)
+        canvas.drawText(content, -textRect.left.toFloat(), cy + ((textRect.bottom - textRect.top) / 2).toFloat(), textPaint)
     }
 
-    private fun drawLine(canvas: Canvas) {
-        val centerX = (canvas.width - linePaint.strokeWidth) / 2
-        canvas.drawLine(centerX, 0f, centerX, height.toFloat(), linePaint)
+    private fun drawLine(canvas: Canvas, cx: Float) {
+        canvas.drawLine(cx, 0f, cx, height.toFloat(), linePaint)
     }
 
-    private fun drawNode(canvas: Canvas) {
-        val interval = (maxValue - minValue) / intervalCount
-        val cx = (canvas.width - linePaint.strokeWidth) / 2
-        var value = maxValue
-        while (value >= minValue) {
-            val cy = height - height * (value / maxValue) + nodeRadius / 2
+    private fun drawNode(canvas: Canvas, cx: Float, cys: List<Float>) {
+        for (cy in cys) {
             canvas.drawCircle(cx, cy, nodeRadius, nodePaint)
-            value -= interval
         }
         // draw current node
         val cy = height - height * (currentValue / maxValue) + currentNodeRadius / 2
         canvas.drawCircle(cx, cy, currentNodeRadius, nodePaint)
     }
 
+    private fun content(float: Float): String {
+        val floatStr = float.toString()
+        val index = floatStr.indexOf('.')
+        return "${floatStr.subSequence(0, (index + 3).coerceAtMost(floatStr.length))}x"
+    }
 
     @SuppressLint("ClickableViewAccessibility")
     override fun onTouchEvent(event: MotionEvent?): Boolean {
@@ -102,7 +128,7 @@ class ScaleIndicator @JvmOverloads constructor(
         when (event.action) {
             ACTION_DOWN -> {
                 isTouching = true
-                setCurrentValue(maxValue * ((height - event.y) / height))
+                setCurrentValue(maxValue * ((height - event.y) / height), false)
                 return true
             }
             ACTION_UP, ACTION_CANCEL -> {
@@ -110,17 +136,37 @@ class ScaleIndicator @JvmOverloads constructor(
                 return true
             }
             ACTION_MOVE -> {
-                setCurrentValue(maxValue * ((height - event.y) / height))
+                setCurrentValue(maxValue * ((height - event.y) / height), false)
                 return true
             }
         }
         return super.onTouchEvent(event)
     }
 
-    private fun setCurrentValue(value: Float) {
+    private fun setCurrentValue(value: Float, smooth: Boolean) {
         val validValue = if (value < minValue) minValue else value
-        this.currentValue = if (validValue > maxValue) maxValue else validValue
-        invalidate()
+        if (smooth) {
+            smoothChangeTo(if (validValue > maxValue) maxValue else validValue)
+        } else {
+            currentValue = if (validValue > maxValue) maxValue else validValue
+            invalidate()
+        }
+    }
+
+    private fun smoothChangeTo(dest: Float) {
+        val oldAnim = animator
+        if (oldAnim != null && oldAnim.isRunning) {
+            oldAnim.cancel()
+        }
+        animator = ValueAnimator.ofFloat(currentValue, dest).apply {
+            addUpdateListener {
+                currentValue = it.animatedValue as Float
+                invalidate()
+            }
+            duration = 100
+            interpolator = DecelerateInterpolator()
+            start()
+        }
     }
 
     companion object {
